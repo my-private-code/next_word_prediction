6b83d83a382d61166d8f83d4088bbe0a7c863ca9
Dong Yuwei <newdongyuwei@gmail.com>
2024-04-21 20:40:51 +0800

    change port to 8080

--- app.py
+++ app.py
@@ -39,4 +39,4 @@ def get_prediction_mask():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=8000, use_reloader=False)
+    app.run(host='0.0.0.0', debug=True, port=8080, use_reloader=False)
